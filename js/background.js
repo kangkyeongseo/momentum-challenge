a53707b8cac2bf440aafdaf6ceb68b45967eadbf
kangkyeongseo <kks_big@naver.com>
2022-04-17 22:06:26 +0900

    Build Background

---
+++ js/background.js
@@ -0,0 +1,12 @@
+const images = [
+    "1.jpg",
+    "2.jpg",
+    "3.jpg"
+];
+
+const seletedImage = images[Math.floor(Math.random()*images.length)];
+
+const background = document.createElement("img");
+background.src = `./images/${seletedImage}`;
+
+document.body.appendChild(background);
